4a1f2b06f28146576a8e1b7f53d8076a8765bb77
Nilaxx_ <nilaxx93@gmail.com>
2025-12-21 01:24:34 +0100

    premier commit

---
+++ MemeAtlasApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemeAtlasApp.swift
+//  MemeAtlas
+//
+//  Created by Suresh on 21/12/2025.
+//
+
+import SwiftUI
+
+@main
+struct MemeAtlasApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
